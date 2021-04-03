3ed1fb16470a30ae2d9904519ca0027b6539c005
chrvillarueldev <christianvillaruel@gmail.com>
2021-04-03 22:13:11 +0800

    Added student package

---
+++ src/main/java/com/student/system/StudentApplication.java
@@ -0,0 +1,14 @@
+package com.student.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.student.system.StudentApplication.class, args);
+    }
+
+}
+
---
+++ src/main/java/com/student/system/student/Gender.java
@@ -0,0 +1,7 @@
+package com.student.system.student;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
