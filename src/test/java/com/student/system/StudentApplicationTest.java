78c4233448a7edc16c0742510f646d8b6eae3f21
chrvillarueldev <christianvillaruel@gmail.com>
2021-04-04 14:14:53 +0800

    Added compilation of react into jar

--- src/test/java/com/student/system/StudentApplicationTest.java
+++ src/test/java/com/student/system/StudentApplicationTest.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.student.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+ class StudentApplicationTest {
 
     @Test
     void contextLoads() {
